5c9846c0c11f0588b65d744f8cf319d310c09bb5
circler3 <circler3@sina.com>
2018-05-20 12:02:21 +0800

    mod

--- ModbusClassic/Program.cs
+++ ModbusClassic/Program.cs
@@ -93,7 +93,7 @@ namespace ModbusClassic
                     }
                     catch
                     {
-
+                        throw;
                     }
                     finally
                     {
